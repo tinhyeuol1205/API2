56e623669041d6290eb1751afb94829bfef6a3ee
12.24 <76689037+tinhyeuol1205@users.noreply.github.com>
2022-05-17 10:54:07 +0700

    Add files via upload

---
+++ Final/src/main/java/tacos/Order.java
@@ -0,0 +1,26 @@
+package tacos;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class Order {
+	private Long id;
+	private Date placedAt;
+	private String name;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	private String ccNumber;
+	private String ccExpiration;
+	private String ccCVV;
+	private List<Taco> tacos ;
+
+}
